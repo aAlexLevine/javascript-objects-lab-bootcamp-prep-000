be40ba32a6fc9b3027957c90a1b6294b219a65c8
Alex Levine <aalexlevine@gmail.com>
2017-09-22 05:01:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 object[key] = value
 return object
 }
+
+function deleteFromObjectByKey(object, key) {
+Object.clone()
+delete object.key
+return object 
+}
