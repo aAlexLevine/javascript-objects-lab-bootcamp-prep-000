e0a92cef855e354a2dc47022d4c4ba1c4f7b7791
Alex Levine <aalexlevine@gmail.com>
2017-09-22 04:02:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return object
+object.key = value
+return object
 }
