d2049211a8545622ca158f32d17521d56737280b
Alex Levine <aalexlevine@gmail.com>
2017-09-22 03:14:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   Object.assign({}, object, { [key]: value} )
-  return {}
+  return object
 }
